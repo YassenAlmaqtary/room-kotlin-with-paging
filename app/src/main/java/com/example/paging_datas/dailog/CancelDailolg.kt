33389f7dd73bc310f8d4fba9405ed1c3072abc83
YassenAlmaqtary <67117546+YassenAlmaqtary@users.noreply.github.com>
2022-03-13 10:24:19 +0300

    Update CancelDailolg.kt

--- app/src/main/java/com/example/paging_datas/dailog/CancelDailolg.kt
+++ app/src/main/java/com/example/paging_datas/dailog/CancelDailolg.kt
@@ -3,15 +3,23 @@ package com.example.paging_datas.dailog
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
+import com.example.paging_datas.R
 import com.google.android.material.textfield.TextInputEditText
 
-class CancelDailolg: DialogFragment() {
+class CancelDailolg: DialogFragment(R.layout.fragment_form_device) {
 
     companion object {
         const val TAG = "PurchaseConfirmationDialog"
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog=AlertDialog.Builder(requireContext()).setMessage("تمكين الالغاء")
         .setPositiveButton("تائكيد"){ _,_ ->
